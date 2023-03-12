497a0e9f8e559fcd30eb7d19d4c9bac308147130
symonk <jackofspaces@gmail.com>
2023-03-12 12:25:07 +0000

    `tox` and `pre-commit` implementation

---
+++ python_cdp/generator.py
@@ -0,0 +1,17 @@
+"""The module responsible for parsing chrome devtools protocol specification
+files into python objects."""
+import argparse
+
+
+def generate() -> int:
+    """The main entry point for python generation."""
+    return 0
+
+
+def handle_commandline_args() -> argparse.Namespace:
+    """Handle command line arguments."""
+    return argparse.ArgumentParser().parse_args()
+
+
+if __name__ == "__main__":
+    raise SystemExit(generate())

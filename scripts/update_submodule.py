69b0d894fcef9828734ea7979bc8104f31393838
symonk <jackofspaces@gmail.com>
2023-03-12 16:43:16 +0000

    :rocket: Updating devtools-protocol submodule

---
+++ scripts/update_submodule.py
@@ -0,0 +1,15 @@
+import subprocess
+
+
+def main() -> int:
+    """A simple utility for automatically upgrading the submodule version of the chrome
+    devtools protocol to the latest.""" 
+    proc = subprocess.run(["git", "submodule", "foreach", "git" "pull" "origin" "master"])
+    if proc.returncode:
+        subprocess.run(["git", "add", "devtools-protocol"])
+        subprocess.run(["git", "commit", "-a", "-m", ":rocket: Updating devtools-protocol submodule"])
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())

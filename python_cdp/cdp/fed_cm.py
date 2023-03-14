6bb61e212f5409743f2699af5d7d14fe9bc3682c
symonk <jackofspaces@gmail.com>
2023-03-14 11:22:56 +0000

    roll protocol `r1116775`

--- python_cdp/cdp/fed_cm.py
+++ python_cdp/cdp/fed_cm.py
@@ -14,6 +14,14 @@ from dataclasses import dataclass
 
 
 @dataclass
+class LoginState:
+    """Whether this is a sign-up or sign-in action for this account, i.e.
+    whether this account has ever been used to sign in to this RP before."""
+
+    ...
+
+
+@dataclass
 class Account:
     """Corresponds to IdentityRequestAccount."""
 
